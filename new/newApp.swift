f7e5e267b4942f026df3dfc0a582a38f7c989f8d
Hillary <hillarytrinh8@gmail.com>
2023-07-14 10:16:35 -0400

    Initial Commit

---
+++ new/newApp.swift
@@ -0,0 +1,17 @@
+//
+//  newApp.swift
+//  new
+//
+//  Created by Melanie Trinh on 2023-07-14.
+//
+
+import SwiftUI
+
+@main
+struct newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
